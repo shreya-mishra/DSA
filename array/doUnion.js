ffa1aebdde18e55952996c14ef72e714f069e01c
shreya-mishra <shreyamishra040398@gmail.com>
2023-09-04 07:03:10 +0530

    fixed issue with union

--- array/doUnion.js
+++ array/doUnion.js
@@ -1,7 +1,6 @@
 export function doUnion(arr1, arr2){
   const filteredArr1 = arr1.filter(item => !arr2.includes(item));
-  const filteredArr2 = arr2.filter(item => !arr1.includes(item));
   
-  const unionArray = [...filteredArr1, ...filteredArr2];
+  const unionArray = [...filteredArr1];
   return unionArray;
 }
